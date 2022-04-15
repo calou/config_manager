a3b6c7e6c725338f6d0cf87f5bb7592992471a51
Sebastien Gruchet <gruchet@gmail.com>
2022-04-15 08:50:03 +0200

    Use  actix services

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
 use std::io;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
 
 use crate::reserved_ports::ReservedPorts;
 
 mod reserved_ports;
 
-#[derive(Clone)]
-struct AppState {
-    reserved_ports: ReservedPorts,
+
+#[get("/next/{port}")]
+async fn next(reserved_ports: web::Data<ReservedPorts>, port: web::Path<u32>) -> impl Responder {
+    let value = reserved_ports.next(Some(port.into_inner()));
+    format!("{}", value)
 }
 
-async fn next(app_state: web::Data<AppState>, port: web::Path<u32>) -> impl Responder {
-    let next = app_state.reserved_ports.next(Some(port.into_inner()));
-    format!("{}", next)
+#[get("/next/reserve/{port}")]
+async fn reserve(reserved_ports: web::Data<ReservedPorts>, port: web::Path<u32>) -> impl Responder {
+    let value = reserved_ports.reserve_next(Some(port.into_inner()));
+    format!("{}", value)
 }
 
-async fn reserve(app_state: web::Data<AppState>, port: web::Path<u32>) -> impl Responder {
-    let next = app_state.reserved_ports.reserve_next(Some(port.into_inner()));
-    format!("{}", next)
-}
-
-async fn release(app_state: web::Data<AppState>, port: web::Path<u32>) -> HttpResponse {
-    app_state.reserved_ports.release(port.into_inner());
+#[get("/next/release/{port}")]
+async fn release(reserved_ports: web::Data<ReservedPorts>, port: web::Path<u32>) -> HttpResponse {
+    reserved_ports.release(port.into_inner());
     HttpResponse::Ok().body("Ok") // FIXME
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let reserved_ports = ReservedPorts::default();
-    let app_state = AppState {
-        reserved_ports
-    };
     HttpServer::new(move || {
-        App::new().app_data(web::Data::new(app_state.clone()))
-            .route("/next/{port}", web::to(next))
-            .route("/next/reserve/{port}", web::to(reserve))
-            .route("/next/release/{port}", web::to(release))
+        App::new().app_data(web::Data::new(reserved_ports.clone()))
+            .service(next)
+            .service(reserve)
+            .service(release)
     }).bind(("127.0.0.1", 3000))?
       .run()
       .await
