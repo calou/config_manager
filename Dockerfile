5f9994738587fca5a7af4a85699aba85fdafa766
Sebastien Gruchet <gruchet@gmail.com>
2022-04-23 21:34:05 +0200

    Add data-dir parameter

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,12 @@ COPY Cargo.toml Cargo.toml
 RUN cargo fetch
 COPY src src
 RUN cargo build --release
+RUN mkdir /data
 
 FROM gcr.io/distroless/cc-debian11
 WORKDIR /app
-
 COPY --from=builder /code/target/release/config-manager config-manager
-
-USER 1001
-
+COPY --from=builder /data /data
 EXPOSE 3000
-
-CMD [ "/app/config-manager" ]
+VOLUME /data
+CMD [ "/app/config-manager", "-d", "/data" ]
--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,26 @@ use actix_web::{App, HttpServer, web};
 use actix_web::middleware::Logger;
 use env_logger::{Env};
 use rocksdb::DB;
+use clap::Parser;
 
 use crate::http::configuration_service;
 use crate::storage::configuration_store::ConfigurationStore;
 use crate::storage::port_store::PortStore;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short='d', long="data-dir")]
+    data_dir: String,
+}
+
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    let path = "/tmp";
+
+    let args = Args::parse();
+    let path = &args.data_dir;
     let directory = Path::new(path);
     let port_store = PortStore::new(DB::open_default(directory.join("ports.db")).unwrap());
     let configuration_store = ConfigurationStore::new(DB::open_default(directory.join("configurations.db")).unwrap());
